c09596447b143a847374ec1e82fe0389fae1bad5
Abdoumo <bedoushop@gmail.com>
2026-05-17 07:28:19 +0100

    test

--- admin_front/src/api/axios.js
+++ admin_front/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'http://localhost:8000/api/v1',
   timeout: 15000,
 });
 
@@ -19,7 +19,7 @@ API.interceptors.response.use(
       orig._retry = true;
       try {
         const rt = localStorage.getItem('refreshToken');
-        const { data } = await axios.post('http://localhost:5000/api/v1/auth/refresh', { refreshToken: rt });
+        const { data } = await axios.post('http://localhost:8000/api/v1/auth/refresh', { refreshToken: rt });
         localStorage.setItem('accessToken', data.accessToken);
         localStorage.setItem('refreshToken', data.refreshToken);
         orig.headers.Authorization = `Bearer ${data.accessToken}`;
