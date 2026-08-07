b3dbf2ead06ae05c6bd33344856a1dbe676f73ac
Abdoumo <bedoushop@gmail.com>
2026-08-07 14:51:52 +0100

    loged out

--- admin_front/src/api/axios.js
+++ admin_front/src/api/axios.js
@@ -46,7 +46,7 @@ API.interceptors.response.use(
 
       try {
         const rt = localStorage.getItem('refreshToken');
-        const { data } = await axios.post('http://localhost:8000/api/v1/auth/refresh', { refreshToken: rt });
+        const { data } = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/auth/refresh`, { refreshToken: rt });
         localStorage.setItem('accessToken', data.accessToken);
         localStorage.setItem('refreshToken', data.refreshToken);
         orig.headers.Authorization = `Bearer ${data.accessToken}`;
